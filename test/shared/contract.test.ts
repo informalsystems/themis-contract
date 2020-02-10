9d47c89d826081203cde1922579203feb95b1409
Thane Thomson <connect@thanethomson.com>
2020-02-10 15:55:20 -0500

    Add basic contract/template parsing

---
+++ test/shared/contract.test.ts
@@ -0,0 +1,75 @@
+import * as assert from 'assert'
+import { Contract } from '../../src/shared/contract'
+import * as tmp from 'tmp'
+import * as path from 'path'
+import { writeFileAsync } from '../../src/shared/async-io'
+
+const TEST_TEMPLATE_CORRECT = `<h1>Correct Contract</h1>
+<p>Effective {{date}}</p>
+
+{{#each counterparties}}
+  <p>Signed by {{this.full_name}}</p>
+  {{#each this.signatories}}
+    <p>{{#has_signed this}}<img src="{{this.signature_image}}">{{else}}Not yet signed{{/has_signed}}</p>
+    <p>{{this.full_names}}</p>
+  {{/each}}
+{{/each}}
+`
+
+const TEST_CONTRACT_CORRECT = `date = "1 January 2020"
+template = "./contract.html"
+counterparties = [
+  "client",
+  "supplier"
+]
+
+[client]
+full_name = "Company XYZ"
+signatories = [
+  "client_sig01"
+]
+
+[client_sig01]
+full_names = "Client Sig01"
+
+[supplier]
+full_name = "ABC Limited"
+signatories = [
+  "supplier_sig01"
+]
+
+[supplier_sig01]
+full_names = "Supplier Sig01"
+`
+
+describe('Contract', () => {
+  describe('#loadFromFile()', () => {
+    it('should parse a well-formed contract and template', async () => {
+      const tmpDir = tmp.dirSync()
+      const tmpTemplate = path.join(tmpDir.name, 'contract.html')
+      const tmpContract = path.join(tmpDir.name, 'contract.toml')
+      await writeFileAsync(tmpTemplate, TEST_TEMPLATE_CORRECT)
+      await writeFileAsync(tmpContract, TEST_CONTRACT_CORRECT)
+      const contract = await Contract.fromFile(tmpContract)
+      tmpDir.removeCallback()
+
+      assert.strictEqual(contract.template.src, tmpTemplate)
+      assert.strictEqual(contract.counterparties.size, 2)
+      const client = contract.counterparties.get('client')
+      if (!client) {
+        throw new Error('Expected "client" counterparty')
+      }
+      assert.strictEqual(client.id, 'client')
+      assert.strictEqual(client.fullName, 'Company XYZ')
+      assert.strictEqual(client.signatories.size, 1)
+
+      const supplier = contract.counterparties.get('supplier')
+      if (!supplier) {
+        throw new Error('Expected "supplier" counterparty')
+      }
+      assert.strictEqual(supplier.id, 'supplier')
+      assert.strictEqual(supplier.fullName, 'ABC Limited')
+      assert.strictEqual(supplier.signatories.size, 1)
+    })
+  })
+})
