e5f607a00333a45ad2f876ba729738b51068cd56
Thane Thomson <connect@thanethomson.com>
2020-02-10 15:54:55 -0500

    Fix async

--- test/shared/counterparties.test.ts
+++ test/shared/counterparties.test.ts
@@ -4,7 +4,7 @@ import * as tmp from 'tmp'
 import * as path from 'path'
 import { writeFileAsync } from '../../src/shared/async-io'
 
-const TEST_COUNTERPARTY = `full_name = "Counterparty A"
+const TEST_COUNTERPARTY_CORRECT = `full_name = "Counterparty A"
 signatories = [
   "sig01",
   "sig02"
@@ -19,31 +19,33 @@ full_names = "Signatory 02"
 keybase_id = "signatory02"
 `
 
-describe('Counterparty', async () => {
-  describe('#loadFromFile()', async () => {
-    const tmpDir = tmp.dirSync()
-    const tmpFilename = path.join(tmpDir.name, 'cp01.toml')
-    await writeFileAsync(tmpFilename, TEST_COUNTERPARTY)
-    const counterparty = await Counterparty.loadFromFile(tmpFilename)
-    tmpDir.removeCallback()
+describe('Counterparty', () => {
+  describe('#loadFromFile()', () => {
+    it('should parse a well-formed counterparty', async () => {
+      const tmpDir = tmp.dirSync()
+      const tmpFilename = path.join(tmpDir.name, 'cp01.toml')
+      await writeFileAsync(tmpFilename, TEST_COUNTERPARTY_CORRECT)
+      const counterparty = await Counterparty.loadFromFile(tmpFilename)
+      tmpDir.removeCallback()
 
-    assert.strictEqual(counterparty.id, 'cp01')
-    assert.strictEqual(counterparty.fullName, 'Counterparty A')
-    assert.strictEqual(counterparty.signatories.size, 2)
-    const sig01 = counterparty.signatories.get('sig01')
-    if (!sig01) {
-      throw new Error('Expected sig01 to be defined')
-    }
-    assert.strictEqual(sig01.id, 'sig01')
-    assert.strictEqual(sig01.fullNames, 'Signatory 01')
-    assert.strictEqual(sig01.keybaseId, 'signatory01')
-    const sig02 = counterparty.signatories.get('sig02')
-    if (!sig02) {
-      throw new Error('Expected sig02 to be defined')
-    }
-    assert.notStrictEqual(sig02, undefined)
-    assert.strictEqual(sig02.id, 'sig02')
-    assert.strictEqual(sig02.fullNames, 'Signatory 02')
-    assert.strictEqual(sig02.keybaseId, 'signatory02')
+      assert.strictEqual(counterparty.id, 'cp01')
+      assert.strictEqual(counterparty.fullName, 'Counterparty A')
+      assert.strictEqual(counterparty.signatories.size, 2)
+      const sig01 = counterparty.signatories.get('sig01')
+      if (!sig01) {
+        throw new Error('Expected sig01 to be defined')
+      }
+      assert.strictEqual(sig01.id, 'sig01')
+      assert.strictEqual(sig01.fullNames, 'Signatory 01')
+      assert.strictEqual(sig01.keybaseId, 'signatory01')
+      const sig02 = counterparty.signatories.get('sig02')
+      if (!sig02) {
+        throw new Error('Expected sig02 to be defined')
+      }
+      assert.notStrictEqual(sig02, undefined)
+      assert.strictEqual(sig02.id, 'sig02')
+      assert.strictEqual(sig02.fullNames, 'Signatory 02')
+      assert.strictEqual(sig02.keybaseId, 'signatory02')
+    })
   })
 })
