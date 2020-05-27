fa1443241a587e10d57fda64b346611d457b93a0
Thane Thomson <connect@thanethomson.com>
2020-05-27 11:11:12 -0400

    Add base CLI structure
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

---
+++ cmd/themis-contract/root.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "themis-contract",
+	Short: "Themis Contract is a tool to help with parameterized legal contracting",
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/informalsystems/themis-contract
 
 go 1.14
+
+require github.com/spf13/cobra v1.0.0 // indirect
